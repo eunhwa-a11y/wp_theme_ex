b4ce59c92996a8ff8b81483a891fbd2b1b53e022
eunhwa-a11y <ttab136@gmail.com>
2024-12-03 10:15:27 +0900

    나만의 검색 form 생성

--- header.php
+++ header.php
@@ -50,7 +50,13 @@
         ) );
       
       ?>
-      <?php get_search_form() ?>
+      <?php
+        // get_search_form()
+      ?>
+      <form action="http://localhost/wordpress/">
+        <input type="search" name="s">
+        <button>검색</button>
+      </form>
     </nav><!-- .main -->
 
     <div class="main">
